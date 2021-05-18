66014f50065b5336701bcc32b2b1202195d9726c
mariagluck <mariamart.berlin@gmail.com>
2021-05-18 13:46:18 +0200

    classes

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 class Employee {
 	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
+		this.firstname = firstname;
+		this.lastname = lastname;		// Complete the code!
 	}
+	fullname() {
+		console.log(`${this.firstname} ${this.lastname}`);
+	}
+	email() {
+		console.log((`${this.firstname}.${this.lastname}@company.com`).toLowerCase());
+	}
+
 }
+
+
+
+
+
+const emp1 = new Employee("John", "Smith");
+console.log(emp1);
+
+emp1.fullname(); // "John Smith"
+emp1.email(); // john.smith@company.com
+console.log(emp1.firstname); //John
+
+const emp2 = new Employee("Mary",  "Sue"); 
+
+emp2.fullname(); // Mary Sue
+emp2.email(); // mary.sue@company.com
+console.log(emp2.firstname); // Mary
+
+const emp3 = new Employee("Antony", "Walker");
+
+emp3.fullname(); // Antony Walker
+emp3.email(); //antony.walker@company.com
+console.log(emp3.firstname); // Antony
